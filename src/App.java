cd566cf72775c200592e1e5eaad71c8e094421f4
brenoloa <contatobrenoloa@gmail.com>
2024-09-15 14:24:35 -0300

    binary

---
+++ src/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //int[][][][] matriz = new int[3][3][3][3];
+
+        for (int i = 0; i < 30; i++) {
+            for (int j = 0; j < 30; j++) {
+                for (int k = 0; k < 30; k++) {
+                    for (int l = 0; l < 30; l++) {
+                        for (int x = 0; x < 30; x++) {
+                            for (int z = 0; z < 30; z++) {
+                                for (int c = 0; c < 30;c++) {
+                                    for (int v = 0; v < 30; v++) {
+                                        System.out.println(i + " " + j + " " + k +" " + l + " " + x + " " + z + " " + c +" " + v + " ");
+                                    
+                                    }
+                                    
+                                }
+                                
+                            }
+                        }
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+}
